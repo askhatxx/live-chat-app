548459cb7ee4e8807f0c98336abcd295ebc99fa1
askhatxx <askhatxx@gmail.com>
2020-07-30 18:35:31 +0600

    Clearing

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LiveChat msg="Welcome"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LiveChat from './components/LiveChat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LiveChat
   }
 }
 </script>
